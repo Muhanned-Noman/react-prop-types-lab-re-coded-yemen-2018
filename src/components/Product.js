3750f8dd076394582825161ab087824c042dd4a3
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-12 14:46:26 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,6 @@
 // Code Product Component Here
 
+<<<<<<< HEAD
 import React,{ Component } from 'react' // We need to import react so we can make use of its .component class
 import PropTypes from 'prop-types'; // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
 
@@ -8,6 +9,22 @@ class Product   extends Component {
     return(
       <div>
         Product
+=======
+import React, { Component } from 'react' // We need to import react so we can make use of its .component class
+import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+
+export class Product   extends Component {
+  render(){
+    return(
+      <div>
+        <ul>
+          <li>this.props.name</li>
+          <li>this.props.producer</li>
+          <li>this.props.hasWatermark</li>
+          <li>this.props.color</li>
+          <li>this.props.weight</li>
+        </ul>
+>>>>>>> 9e72720d6c05df3c0371ab5774f2605ec568ba17
       </div>
     )
   }
@@ -22,6 +39,7 @@ Product.propTypes = {
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
   color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+<<<<<<< HEAD
   weight:(props, propName) => {
     const weight = props[propName];
 
@@ -41,3 +59,18 @@ Product.propTypes = {
   },
 }
 export default Product;
+=======
+  weight: withinRange.isRequired
+};
+
+function withinRange(props, propName, componentName) {
+  componentName = comopnentName || 'ANONYMOUS';
+  if (props[propName]) {
+    let value = props[propName];
+    if (typeof value === 'number') {
+        return (value >= 80  && value <= 300) ? null : new Error(propName + ' in ' + componentName + " is not within 80 to 300");
+    }
+  }
+  return null;
+}
+>>>>>>> 9e72720d6c05df3c0371ab5774f2605ec568ba17
